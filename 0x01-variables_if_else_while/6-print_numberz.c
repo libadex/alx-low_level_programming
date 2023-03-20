5e1d4be39323f5d834fc610c20e6fc6105e07bc6
libadex <odohdestiny@gmail.com>
2023-03-20 15:14:20 -0700

    print tebaalpha

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - Prints all single digit numbers of base 10 starting from 0,
+ * main - Print all single digit numbers of base 10 starting from 0,
  *        only using putchar and without char variables.
  *
  * Return: Always 0.
@@ -12,8 +12,6 @@ int main(void)
 
 	for (num = 0; num < 10; num++)
 		putchar((num % 10) + '0');
-
-	putchar('\n');
-
-	return (0);
+		putchar('\n');
+		return (0);
 }
